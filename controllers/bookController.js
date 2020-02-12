f5576d27c255b99d49789c08a5d577c3e64e4245
SuzannAdams <suzannyee@yahoo.com>
2020-02-12 12:53:27 -0800

    backend routes complete

---
+++ controllers/bookController.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const Book = require('../db/models/Book');
+
+router.get('/', (req, res) => {
+  Book.find({}).then(books => {
+    res.json(books);
+  });
+});
+router.get('/:id', (req, res) => {
+  Book.findById(req.params.id).then(book => {
+    res.json(book);
+  });
+});
+router.post('/', (req, res) => {
+  Book.create(req.body).then(book => {
+    res.json(book);
+  });
+});
+router.put('/:id', (req, res) => {
+  Book.findByIdAndUpdate(req.params.id).then(book => {
+    res.json(book);
+  });
+});
+router.delete('/:id', (req, res) => {
+  Book.findByIdAndDelete(req.params.id).then(book => {
+    res.json(book);
+  });
+});
+module.exports = router;
--- db/models/Book.js
+++ db/models/Book.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require('../connection');
 
 const BookSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		required: true
-	},
-	author: {
-		type: String,
-		required: true
-	},
-	genre: {
-		type: String,
-		required: true
-	},
-	rating: {
-		type: Number,
-		required: true,
-		min: 0,
-		max: 5
-	}
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5
+  }
 });
 
 const Book = mongoose.model('Book', BookSchema);
