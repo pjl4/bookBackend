655dde6b8f7c978493dc0eab152fcbac2646a1fd
pjl4 <pj.lutz17@gmail.com>
2020-02-12 16:33:45 -0500

    fixed cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const bookController = require('./controllers/bookController');
+const cors = require('cors');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api/books', bookController);
 app.listen(8080, () => {
-  console.log('app runnning on 8080');
+	console.log('app runnning on 8080');
 });
