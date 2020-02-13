c3b27dbd70094f0f6c2d783e07e1c94f44c5e1f1
pjl4 <pj.lutz17@gmail.com>
2020-02-13 11:41:41 -0500

    added editing component

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,28 +3,28 @@ const router = express.Router();
 const Book = require('../db/models/Book');
 
 router.get('/', (req, res) => {
-  Book.find({}).then(books => {
-    res.json(books);
-  });
+	Book.find({}).then((books) => {
+		res.json(books);
+	});
 });
 router.get('/:id', (req, res) => {
-  Book.findById(req.params.id).then(book => {
-    res.json(book);
-  });
+	Book.findById(req.params.id).then((book) => {
+		res.json(book);
+	});
 });
 router.post('/', (req, res) => {
-  Book.create(req.body).then(book => {
-    res.json(book);
-  });
+	Book.create(req.body).then((book) => {
+		res.json(book);
+	});
 });
 router.put('/:id', (req, res) => {
-  Book.findByIdAndUpdate(req.params.id).then(book => {
-    res.json(book);
-  });
+	Book.findByIdAndUpdate(req.params.id, req.body).then((book) => {
+		res.json(book);
+	});
 });
 router.delete('/:id', (req, res) => {
-  Book.findByIdAndDelete(req.params.id).then(book => {
-    res.json(book);
-  });
+	Book.findByIdAndDelete(req.params.id).then((book) => {
+		res.json(book);
+	});
 });
 module.exports = router;
